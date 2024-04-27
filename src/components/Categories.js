7cb01f964be0b18e1e9f7f134286a934f02f2ed0
Srivarshini <srivarshini.vs@gmail.com>
2024-04-27 12:19:21 +1000

    Assignment 2 Milestone 1 Initial Commits to Establish the Screens and Navigation

---
+++ src/components/Categories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Alert, ActivityIndicator } from 'react-native';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontSize: 16,
+        color: 'gray',
+    },
+});
+
+const Home = () => {
+    return (
+        <SafeAreaView style={styles.root}>
+            <View style={styles.container}>
+                <Text style={styles.title}>Categorie Tab!</Text>
+                <Text style={styles.subtitle}>Browse our products and start shopping.</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default Home;
---
+++ src/components/TabNavigation.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Home from './Home';
+import Categories from './Categories';
+
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MyTabs =() => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#e91e63"
+      barStyle={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Categories"
+        component={Categories}
+        options={{
+          tabBarLabel: 'Categories',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default MyTabs
